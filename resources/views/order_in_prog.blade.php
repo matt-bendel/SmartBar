0296e874aad421b53ee4fd5262c0f03b9c3e433a
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2020-07-20 21:16:28 -0400

    Fixed issue with view

--- resources/views/order_in_prog.blade.php
+++ resources/views/order_in_prog.blade.php
@@ -2,13 +2,6 @@
 <div style="width: 80%; margin-left: 10%; margin-right: 10%;">
     <p style="font-size: 70px; text-align: left; color: white; font-weight: bold">Thank you for you order!</p>
     <p style="font-size: 25px; color: white">Please allow SmartBar a few moments to process and fulfill your order.</p>
-    <script>
-        Echo.channel('orders.' + {{$order->id}}).listen('OrderUpdated', (e) => {
-            this.message = e.message;
-            this.order.status = e.order.status;
-            console.log(e);
-        });
-    </script>
 {{--    <div style="height: 150px; width: 150px; margin-left: 55%">--}}
 {{--        <div class="loader"></div>--}}
 {{--    </div>--}}
